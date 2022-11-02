f3b58887008bc5831def3c20ffe03e5d57ff566c
unknown <eodnjs9605@gmail.com>
2022-11-02 17:55:20 +0900

    1102

--- src/components/section/ImgText.js
+++ src/components/section/ImgText.js
@@ -3,6 +3,7 @@ const imgTextTitle = {
   title: "24시간 언제 어디서나 신선하게",
   desc: "‘콜드체인 배달 시스템’에 따라 신선도를 유지해 안전하게 배송되며,온도 모니터링 기능이 가능한 스마트 디바이스로 현장관리자 없이 24시간운영 가능한 비대면 플랫폼 입니다.",
 };
+ddd;
 const ImgTextDesc = [
   {
     img: "assets/img/imgText_bg01.jpg",
